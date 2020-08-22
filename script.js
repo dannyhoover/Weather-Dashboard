6e536cb75134ab35fa757e64891d38d8587a89b1
dannyhoover <dofjrtd@gmail.com>
2020-08-22 12:45:52 -0700

    Finishing touches pending

--- script.js
+++ script.js
@@ -17,17 +17,22 @@ searchForm.on("submit", function (event) {
 // async function that displays the current city weather items
 async function displayCityWeather(city) {
     const apiKey = "2aa88321525eb31701d7a4a3a6a53204";
-    var lat = 37.75;
-    var lon = -122.37;
     // call for responses of weather and forecast api's
     try {
         const responses = await Promise.all([
             fetch(`https://api.openweathermap.org/data/2.5/weather?appid=${apiKey}&q=${city}`),
             fetch(`https://api.openweathermap.org/data/2.5/forecast?appid=${apiKey}&q=${city}`),
+        ]);
+        const [weather, forecast] = await Promise.all(responses.map(response => response.json()));
+        console.log(weather, forecast);
+        var lat = weather.coord.lat;
+        var lon = weather.coord.lon;
+        const response = await Promise.all([
             fetch(`https://api.openweathermap.org/data/2.5/uvi?appid=${apiKey}&lat=${lat}&lon=${lon}`)
         ]);
-        const [weather, forecast, uvindex] = await Promise.all(responses.map(response => response.json()));
-        console.log(weather, forecast, uvindex);
+        const uvindex = await Promise.all(response.map(response => response.json()));
+        console.log(uvindex);
+        
         weatherDisplay.empty();
         forecastDisplay.empty();
 
@@ -42,20 +47,18 @@ async function displayCityWeather(city) {
             const temperatureText = $("<p>").addClass("card-text").text(`Temperature: ${weather.main.temp} kelvin`);
             const humidityText = $("<p>").addClass("card-text").text(`Humidity: ${weather.main.humidity}`);
             const windText = $("<p>").addClass("card-text").text(`Wind Speed: ${weather.wind.speed}`);
-            const uvText = $("<p>").addClass("card-text").text(`UV Index: ${uvindex.value}`);
+            const uvText = $("<p>").addClass("card-text").text(`UV Index: ${uvindex[0].value}`);
             cardBody.append(cardTitle, temperatureText, humidityText, windText, uvText);
 
             // loop that puts forecast data on the screen
-            for (let index = 7; index <= 39; index++) {
+            for (let index = 7; index <= 39; index+=8) {
                 const forecastItem = $("<div>").addClass("#card text-white bg-primary mb-3").appendTo(forecastDisplay);
                 const forecastBody = $("<div>").addClass("#forecast-card-body").appendTo(forecastItem);
                 const forecastTitle = $("<p>").text(`Date: ${forecast.list[index].dt_txt}`);
-                const forecastLogo = $("<img>").attr("src", `https://openweathermap.org/img/wn/${weather.weather[0].icon}.png`);
+                const forecastLogo = $("<img>").attr("src", `https://openweathermap.org/img/wn/${forecast.list[index].weather[0].icon}.png`);
                 const fortempText = $("<p>").text(`Temp: ${forecast.list[index].main.temp_max} kelvin`);
                 const forehumText = $("<p>").text(`Humidity: ${forecast.list[index].main.humidity}`);
-
                 forecastBody.append(forecastTitle, forecastLogo, fortempText, forehumText);
-                
             }
         }
 
